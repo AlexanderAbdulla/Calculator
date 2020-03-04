941a489bc71c939439f11c074fc1cf9553582106
Alex Abdulla <alexander.amadeus.abdulla@gmail.com>
2020-03-04 15:35:11 -0800

    Addition / Subtraction With Formula

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -4,28 +4,32 @@ export default class Calculator extends LightningElement {
     currentDisplay = "";
     numberToOperate = "";
     latestOperation = "";
+    formula = "";
     enterNumber(event) {
         this.currentDisplay += event.target.dataset.id;
     }
     enterOperation(event) {
         switch(event.target.dataset.id) {
             case 'a':
-                console.log('addition');
+                console.log('addition')
                 this.numberToOperate = this.currentDisplay;
                 this.latestOperation = 'a';
                 this.currentDisplay = "";
-              break;
+                this.formula += this.numberToOperate  + "+";
+                break;
             case 'b':
-              console.log('subtraction');
-              this.numberToOperate = this.currentDisplay;
-              this.latestOperation = 'b';
-              this.currentDisplay = "";
-              break;
+                console.log('subtraction');
+                this.numberToOperate = this.currentDisplay;
+                this.latestOperation = 'b';
+                this.currentDisplay = "";
+                this.formula += this.numberToOperate  + "-";
+                break;
             case 'd':
                 console.log('clearing');
                 this.numberToOperate = "";
                 this.latestOperation = "";
                 this.currentDisplay = "";
+                this.formula = "";
                 break;
             default:
               // code block
@@ -35,12 +39,16 @@ export default class Calculator extends LightningElement {
         console.log('equals');
         switch(this.latestOperation) {
             case 'a':
+                this.formula += this.currentDisplay;
                 this.currentDisplay = parseInt(this.numberToOperate) +  parseInt(this.currentDisplay);
+                this.formula += "=" + this.currentDisplay;
                 this.numberToOperate  = this.currentDisplay;
                 break;
             case 'b':
+                this.formula += this.currentDisplay;
                 this.currentDisplay = parseInt(this.numberToOperate) - parseInt(this.currentDisplay);
                 this.numberToOperate  = this.currentDisplay;
+                this.formula += "=" + this.currentDisplay;
                 break;
             default: 
         }
