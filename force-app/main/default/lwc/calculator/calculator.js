98ee57e61a09ac400b81f3045a80836491cdd333
Alex Abdulla <alexander.amadeus.abdulla@gmail.com>
2020-03-04 16:28:17 -0800

    Added Disabling Of Btns
    So there is a little bit of dom manipulation going on here using the template object we get in LWC.
    
    TODO: Mutliply Fx / Divide Fx / Equals bug

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -16,6 +16,7 @@ export default class Calculator extends LightningElement {
                 this.latestOperation = 'a';
                 this.currentDisplay = "";
                 this.formula += this.numberToOperate  + "+";
+                this.closeOperations();
                 break;
             case 'b':
                 console.log('subtraction');
@@ -23,6 +24,7 @@ export default class Calculator extends LightningElement {
                 this.latestOperation = 'b';
                 this.currentDisplay = "";
                 this.formula += this.numberToOperate  + "-";
+                this.closeOperations();
                 break;
             case 'd':
                 console.log('clearing');
@@ -30,6 +32,7 @@ export default class Calculator extends LightningElement {
                 this.latestOperation = "";
                 this.currentDisplay = "";
                 this.formula = "";
+                this.openOperations();
                 break;
             default:
               // code block
@@ -42,15 +45,38 @@ export default class Calculator extends LightningElement {
                 this.formula += this.currentDisplay;
                 this.currentDisplay = parseInt(this.numberToOperate) +  parseInt(this.currentDisplay);
                 this.formula += "=" + this.currentDisplay;
-                this.numberToOperate  = this.currentDisplay;
+                this.numberToOperate = this.currentDisplay;
+                this.openOperations();
                 break;
             case 'b':
                 this.formula += this.currentDisplay;
                 this.currentDisplay = parseInt(this.numberToOperate) - parseInt(this.currentDisplay);
                 this.numberToOperate  = this.currentDisplay;
                 this.formula += "=" + this.currentDisplay;
+                this.openOperations();
                 break;
             default: 
         }
+    }
+    
+    closeOperations(){
+        var buttons = this.template.querySelectorAll('button');
+        [].forEach.call(buttons, function(button){
+            if((button.dataset.id == 'a') || (button.dataset.id == 'b') || (button.dataset.id == 'c')
+            || (button.dataset.id == 'f')) {
+                    button.disabled = true;
+            }
+        });
+    }
+
+    openOperations(){
+        console.log("opening operations");
+        var buttons = this.template.querySelectorAll('button');
+        [].forEach.call(buttons, function(button){
+            if((button.dataset.id == 'a') || (button.dataset.id == 'b') || (button.dataset.id == 'c')
+            || (button.dataset.id == 'f')) {
+                    button.disabled = false;
+            }
+        });
     }
 }
